bb37180d963c88e90976cf23657a7cf6ef56d712
Vova <paprotskyv@gmail.com>
2024-08-01 18:57:35 +0200

    Initial commit

---
+++ src/test/java/org/example/songify/SongifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.songify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
