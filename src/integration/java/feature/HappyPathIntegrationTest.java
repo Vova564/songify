30bdf2bffd732323d4b3d04a9895401f4d946ce2
vova <paprotskyv@gmail.com>
2024-10-02 16:38:06 +0200

    Removed pgadmin from docker compose and changed db version to fixed 15. Added testcontainers db connection

---
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -0,0 +1,32 @@
+package feature;
+
+import org.example.songify.SongifyApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(classes = SongifyApplication.class)
+@Testcontainers
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+class HappyPathIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    }
+
+    @Test
+    public void test() {
+    }
+
+}
